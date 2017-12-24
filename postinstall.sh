2bd2fe91514d2432109872a8b8aaa481b81a194b
Auston Stewart <auston@shedbuilt.net>
2017-12-23 16:48:36 -1000

    Temporarily disable mkswap, as if fails in chroot from a system where /var/swap is in use

--- postinstall.sh
+++ postinstall.sh
@@ -2,4 +2,5 @@
 # Set up swapfile
 dd if=/dev/zero of=/var/swap bs=1M count=512
 chmod 600 /var/swap
-mkswap /var/swap
+# Temporarily disabled as this fails in chroot on systems where /var/swap is in use
+# mkswap /var/swap
